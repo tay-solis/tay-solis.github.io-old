87a471d7cd97c0faa4d0e7ac5dac0bda95638ad2
Tay J Solis <tayjsolis@gmail.com>
2018-06-29 13:17:01 -0700

    one more try

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+var slides = getSlides(href);
+var href = document.location.pathname;
+
+function getSlides(href){
+  var slides = [];
+  switch (href){
+    case "http://tayjsolis.com/art/homo.html":
+      slides = [
+        "https://res.cloudinary.com/tayjsolis/image/upload/v1522699906/09011603.jpg.jpg",
+        "https://res.cloudinary.com/tayjsolis/image/upload/v1522699611/homo_web.jpg",
+        "https://res.cloudinary.com/tayjsolis/image/upload/v1522699863/07081602.jpg",
+        "https://res.cloudinary.com/tayjsolis/image/upload/v1522699987/smol.jpg",
+        "https://res.cloudinary.com/tayjsolis/image/upload/v1522699877/07081607.jpg",
+        "https://res.cloudinary.com/tayjsolis/image/upload/v1522699982/meet_peep_web.jpg",
+        "https://res.cloudinary.com/tayjsolis/image/upload/v1522699974/meet_peep_6.jpg",
+        "https://res.cloudinary.com/tayjsolis/image/upload/v1522699890/meep_peep_1.jpg"
+      ];
+      break;
+    case "wanderlust.html":
+      slides = [
+        "https://res.cloudinary.com/tayjsolis/image/upload/v1522699787/exhibit_poster_art_printing.jpg"
+      ];
+      break;
+  }
+  return slides;
+
+}
+
+var currentSlide = 0;
+
+
 $(document).ready(function(){
     $('.welcome').fadeIn(1000).css('display', 'block');
     $('.container').fadeIn(1000).css('display', 'block');
@@ -12,4 +43,31 @@ $('.dropdown').click(function(){
 
 $('.hamburger').click(function(){
   $('.navCollapse').slideToggle();
+});
+
+$(window).resize(function(){
+  if($(window).width() <= 800){
+    currentSlide = 0;
+    $('.slides').children('#show').attr('src', slides[currentSlide]);
+  }
+});
+
+$('.prev').click(function(){
+  if(currentSlide === 0){
+    currentSlide = slides.length - 1;
+    $('.slides').children('#show').attr('src', slides[currentSlide]);
+  } else{
+    currentSlide = currentSlide - 1;
+    $('.slides').children('#show').attr('src', slides[currentSlide]);
+  }
+});
+
+$('.next').click(function(){
+  if(currentSlide === slides.length - 1){
+    currentSlide = 0;
+    $('.slides').children('#show').attr('src', slides[currentSlide]);
+  } else{
+    currentSlide = currentSlide + 1;
+    $('.slides').children('#show').attr('src', slides[currentSlide]);
+  }
 });
